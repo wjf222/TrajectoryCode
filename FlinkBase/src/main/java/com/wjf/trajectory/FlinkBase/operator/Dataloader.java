af3f37f3883297fc1b223811c4415ee7aebad41b
wjf <jffwang@qq.com>
2023-12-24 18:09:46 +0800

    fix：优化代码

--- FlinkBase/src/main/java/com/wjf/trajectory/FlinkBase/operator/Dataloader.java
+++ FlinkBase/src/main/java/com/wjf/trajectory/FlinkBase/operator/Dataloader.java
@@ -2,13 +2,15 @@ package com.wjf.trajectory.FlinkBase.operator;
 
 import entity.TracingPoint;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.runtime.metrics.dump.MetricQueryService;
 import org.apache.flink.util.Collector;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
-public class Dataloader implements FlatMapFunction<String, TracingPoint> {
+public class Dataloader extends RichFlatMapFunction<String, TracingPoint> {
     @Override
     public void flatMap(String line, Collector<TracingPoint> out) throws Exception {
         String[] strs = line.split(",");
@@ -19,11 +21,37 @@ public class Dataloader implements FlatMapFunction<String, TracingPoint> {
         double y = Double.parseDouble(strs[3]);
         if (y < 39.5) y = 39.5;
         else if (y > 40.3) y = 40.3;
-        out.collect(new TracingPoint(x, y, dateToTimestamp(strs[1], "yyyy-MM-dd HH:mm:ss"),id));
+
+        TracingPoint point = TracingPoint.builder()
+                .id(id)
+                .date(dateToTimestamp(strs[1], "yyyy-MM-dd HH:mm:ss"))
+                .x(x)
+                .y(y)
+                .shardKey(generateShardKey(id))
+                .build();
+        out.collect(point);
     }
 
+    public long generateShardKey(long id){
+        return id % 8;
+    }
     public long dateToTimestamp(String date, String pattern) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
         return LocalDateTime.parse(date, formatter).toInstant(ZoneOffset.ofHours(8)).toEpochMilli();
     }
+
+//    public int getMetric(){
+//
+//        String jobId = String.valueOf(getRuntimeContext().getJobId());
+//        getRunt
+//        String jobManagerAddress = "http://localhost:8081"; // Flink JobManager 地址和端口
+//
+//        // 构建 REST API 请求 URL
+//        String restUrl = jobManagerAddress + "/jobs/" + jobId + "/metrics";
+//
+//        // 发送 HTTP GET 请求获取指标信息
+//        HttpURLConnection conn = (HttpURLConnection) new URL(restUrl).openConnection();
+//        conn.setRequestMethod("GET");
+//
+//    }
 }
---
+++ FlinkBase/src/main/java/com/wjf/trajectory/FlinkBase/operator/range/PartitionXZRangeQueryProcess.java
@@ -0,0 +1,77 @@
+package com.wjf.trajectory.FlinkBase.operator.range;
+
+import com.github.davidmoten.rtree.RTree;
+import com.github.davidmoten.rtree.geometry.Geometries;
+import com.github.davidmoten.rtree.geometry.Rectangle;
+import entity.TracingPoint;
+import entity.TracingQueue;
+import indexs.IndexRange;
+import indexs.commons.Window;
+import indexs.z2.XZ2SFC;
+import org.apache.flink.api.common.state.MapState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.metrics.Counter;
+import org.apache.flink.metrics.MetricGroup;
+import org.apache.flink.runtime.state.KeyedStateFunction;
+import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
+import org.apache.flink.streaming.api.functions.co.KeyedBroadcastProcessFunction;
+import org.apache.flink.util.Collector;
+
+import java.util.*;
+
+public class PartitionXZRangeQueryProcess extends BroadcastProcessFunction<TracingPoint, Window, Tuple2<Window,List<Long>>> {
+    private transient Counter calculateStrength;
+    private Map<Long, TracingQueue> idTrajectoryMap;
+    private final XZ2SFC xz2sfc;
+
+    public PartitionXZRangeQueryProcess(XZ2SFC xz2SFC) {
+        this.xz2sfc = xz2SFC;
+    }
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        idTrajectoryMap = new HashMap<>();
+        // Access Flink's MetricGroup
+        MetricGroup metricGroup = getRuntimeContext().getMetricGroup();
+        // Create a separate MetricGroup for the function
+        MetricGroup functionMetricGroup = metricGroup.addGroup("Trajectory-Range");
+
+        // Register a new Counter metric for the function
+        calculateStrength = functionMetricGroup.counter(String.format("customCounter-%s",getRuntimeContext().getIndexOfThisSubtask()));
+        System.out.printf("Metric:%s\tSubTask:%s\r\n",calculateStrength.toString(),getRuntimeContext().getIndexOfThisSubtask());
+    }
+
+    @Override
+    public void processElement(TracingPoint point, BroadcastProcessFunction<TracingPoint, Window, Tuple2<Window, List<Long>>>.ReadOnlyContext ctx, Collector<Tuple2<Window, List<Long>>> out) throws Exception {
+        TracingQueue trajectory = idTrajectoryMap.get(point.id);
+        if(trajectory == null) {
+            System.out.printf("Metric:%s\tSubTaskId:%d\trest:%d\tshardKey:%d\r\n",calculateStrength.toString(),getRuntimeContext().getIndexOfThisSubtask(),point.id%8,point.shardKey);
+            trajectory = new TracingQueue();
+        }
+        trajectory.EnCircularQueue(point);
+        trajectory.updateIndex(xz2sfc);
+        idTrajectoryMap.put(point.getId(),trajectory);
+    }
+
+    @Override
+    public void processBroadcastElement(Window window, BroadcastProcessFunction<TracingPoint, Window, Tuple2<Window, List<Long>>>.Context ctx, Collector<Tuple2<Window, List<Long>>> out) throws Exception {
+        List<Window> list = Collections.singletonList(window);
+        List<IndexRange> ranges = this.xz2sfc.ranges(list, Optional.empty());
+        List<Long> result = new ArrayList<>();
+        for(Map.Entry<Long, TracingQueue> trajectoryEntry: idTrajectoryMap.entrySet()) {
+            for (IndexRange range:ranges) {
+                if(range.intersect(trajectoryEntry.getValue().getIndex())) {
+                    result.add(trajectoryEntry.getKey());
+                    // 已经和一个范围相交,不需要继续判断
+                    break;
+                }
+            }
+        }
+        out.collect(Tuple2.of(window,result));
+    }
+}
